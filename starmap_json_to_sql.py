64c7fb06a2ca9bcd4f634ce98794ac803e1d7a45
Brian Rossman <berossm@gmail.com>
2016-09-25 01:06:47 -0400

    affiliation and systems sql files verified

--- starmap_json_to_sql.py
+++ starmap_json_to_sql.py
@@ -4,6 +4,15 @@ import os
 import sys
 import MySQLdb
 import json
+from unidecode import unidecode
+
+
+def value_or_none(json_data):
+    if json_data == None:
+        return str(0.00000000)
+    else:
+        return MySQLdb.escape_string(str(json_data))
+
 base_path = "./starmap"
 sql_path = "./sql"
 data_file = "bootup"
@@ -34,17 +43,17 @@ with open(sql_path + "/affiliation.sql", 'w') as sql_file:
               "VARCHAR(64) NOT NULL , `color` VARCHAR(8) NOT NULL , " +\
               "PRIMARY KEY (`id`)) ENGINE = InnoDB;\n"
     sql_file.write(sql_str)
-    sql_str = "INSERT INTO " + sql_table + \
-              " (`id`, `code`, `name`, `color`) VALUES "
-    sql_rows = "('"
+    sql_base = "INSERT INTO " + sql_table + \
+               " (`id`, `code`, `name`, `color`) VALUES "    
     for affiliation in base_data["data"]["affiliations"]["resultset"]:
-        sql_rows += MySQLdb.escape_string(affiliation["id"])
-        sql_rows += "', '" + MySQLdb.escape_string(affiliation["code"])
-        sql_rows += "', '" + MySQLdb.escape_string(affiliation["name"])
-        sql_rows += "', '" + MySQLdb.escape_string(affiliation["name"])[-6:]
-        sql_rows += "), ('"
-    sql_str += sql_rows[:-3] + ";\n"
-    sql_file.write(sql_str)
+        sql_row = "('"
+        sql_row += MySQLdb.escape_string(affiliation["id"])
+        sql_row += "', '" + MySQLdb.escape_string(affiliation["code"])
+        sql_row += "', '" + MySQLdb.escape_string(affiliation["name"])
+        sql_row += "', '" + MySQLdb.escape_string(affiliation["color"])[-6:]
+        sql_row += "')"
+        sql_str = sql_base + sql_row + ";\n"
+        sql_file.write(sql_str)
 
 with open(sql_path + "/systems.sql", 'w') as sql_file:
     sql_table = table_base + "systems`"
@@ -62,35 +71,36 @@ with open(sql_path + "/systems.sql", 'w') as sql_file:
               "REAL NOT NULL , `time_modified` TIMESTAMP NOT NULL , " +\
               "PRIMARY KEY (`id`)) ENGINE = InnoDB;\n"
     sql_file.write(sql_str)
-    sql_str = "INSERT INTO " + sql_table + \
-              " (`id`, `code`, `name`, `description`, `type`, " +\
-              "`position_z`, `position_x`, `position_y`, `affiliation`, " +\
-              "`status`, `habitable_zone_inner` , `habitable_zone_outer` , " +\
-              "`frost_line` , `aggregated_size`, `aggregated_population`, " +\
-              "`aggregated_economy`, `aggregated_danger`, `time_modified`) VALUES " 
-    sql_rows = "(''"
+    sql_base = "INSERT INTO " + sql_table + \
+               " (`id`, `code`, `name`, `description`, `type`, " +\
+               "`position_z`, `position_x`, `position_y`, `affiliation`, " +\
+               "`status`, `habitable_zone_inner` , `habitable_zone_outer` , " +\
+               "`frost_line` , `aggregated_size`, `aggregated_population`, " +\
+               "`aggregated_economy`, `aggregated_danger`, `time_modified`) VALUES "     
     for system_file in os.listdir(base_path + "/star-systems/"):
         if system_file.endswith(".json"):
             with open(base_path + "/star-systems/"+system_file, 'r') as system_json:
                 system_data = json.load(system_json)["data"]["resultset"][0]
+                sql_rows = "('"
                 sql_rows += MySQLdb.escape_string(system_data["id"])
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["code"])
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["name"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["description"])
+                decoded_description = unidecode(system_data["description"])
+                sql_rows += "', '" + MySQLdb.escape_string(decoded_description)
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["type"])
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["position_z"])
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["position_x"])
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["position_y"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["affiliation"])
+                sql_rows += "', '" + MySQLdb.escape_string(system_data["affiliation"][0]["id"])
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["status"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["habitable_zone_inner"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["habitable_zone_outer"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["frost_line"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["aggregated_size"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["aggregated_population"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["aggregated_economy"])
-                sql_rows += "', '" + MySQLdb.escape_string(system_data["aggregated_danger"])
+                sql_rows += "', '" + value_or_none(system_data["habitable_zone_inner"])
+                sql_rows += "', '" + value_or_none(system_data["habitable_zone_outer"])
+                sql_rows += "', '" + value_or_none(system_data["frost_line"])
+                sql_rows += "', '" + value_or_none(system_data["aggregated_size"])
+                sql_rows += "', '" + value_or_none(system_data["aggregated_population"])
+                sql_rows += "', '" + value_or_none(system_data["aggregated_economy"])
+                sql_rows += "', '" + value_or_none(system_data["aggregated_danger"])
                 sql_rows += "', '" + MySQLdb.escape_string(system_data["time_modified"])
-                sql_rows += "), ('"
-    sql_str += sql_rows[:-3] + ";\n"
-    sql_file.write(sql_str)
+                sql_rows += "')"
+                sql_str = sql_base + sql_rows + ";\n"
+                sql_file.write(sql_str)
